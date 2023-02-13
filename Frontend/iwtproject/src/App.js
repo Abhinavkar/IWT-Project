46b118488ed3bace0578b2c5ff9019b0b5c68096
Abhinav kar <75711413+Abhinavkar@users.noreply.github.com>
2023-02-13 09:46:12 +0000

    ADded routes

--- Frontend/iwtproject/src/App.js
+++ Frontend/iwtproject/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {createdBrowserRouter, RouterProvider} from 'react-router-dom'
 
 export default function App() {
+  const router = createdBrowserRouter([
+
+      {
+        path : '/'
+        element : <div> Root Route </div>
+      }
+  ])
   return (
  
     <h1 className="text-3xl font-bold underline">
